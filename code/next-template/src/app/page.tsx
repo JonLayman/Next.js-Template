52d63107fd698b6049db26fc91ef21aab5581bb6
Jono Layman <J.layman910@gmail.comgit config --global user.name Jon>
2024-05-04 17:43:23 -0400

    Initial Setup

---
+++ code/next-template/src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Home.css";
+
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font bold">Next JS Starter Template</h1>
+      </div>
+    </section>
+  );
+}
